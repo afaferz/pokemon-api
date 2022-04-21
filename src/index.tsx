3e9fdd03178bf27edaac5712cacdfce71f15e78f
afaferz <afa00.papa@gmail.com>
2022-04-20 22:33:19 -0300

    feat: Config theme provider and Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+import theme from './common/theme/theme';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
+import Home from './pages/home';
+import Pokemon from './pages/pokemon';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+// New way to render
+const app = document.getElementById('root');
+const root = createRoot(app!);
+
+const router = (
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<Home />} />
+            <Route path="/pokemon" element={<Pokemon />}>
+                <Route path=":pokemonId"></Route>
+            </Route>
+            <Route path="quizz"></Route>
+        </Routes>
+        <App />
+    </BrowserRouter>
+);
+
+root.render(
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>{router}</Provider>
+        </ThemeProvider>
+    </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
